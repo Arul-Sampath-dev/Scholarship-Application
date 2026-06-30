92221649d7680fc93b38aff7e9f262703e33bbb1
ARUL S <arulsampathcyr@gmail.com>
2026-06-30 11:40:48 +0530

    Created Domain Driven Exceptions and Global Exception Handler, Exception-Registry for Map status codes with respected exceptions

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from starlette.middleware.sessions import SessionMiddleware
 
+from src.api.exception_registry import exception_registry
 from src.api.routes.authentication import authentication_router
-from src.exceptions import InvalidCredentials
+from src.exceptions import DomainException
 
 app = FastAPI()
 
@@ -21,7 +24,22 @@ app.include_router(authentication_router)
 
 
 # adding universal exception handlers
-#
-@app.exception_handler(InvalidCredentials)
-def invalid_credentials_handler(request: Request, exc: InvalidCredentials):
-    return JSONResponse(status_code=401, content={"detail": "Invalid credentials"})
+#  Handling the global exception Here i handle all domain exceptions because all exceptions comes under domain execptions
+
+
+@app.exception_handler(DomainException)
+def domain_exception_handler(request: Request, exc: DomainException) -> JSONResponse:
+    status_code = HTTPStatus.INTERNAL_SERVER_ERROR  # default status code = 500
+
+    for exception_type, status in exception_registry.items():
+        if isinstance(exc, exception_type):
+            status_code = status
+            break
+
+    return JSONResponse(
+        status_code=status_code,
+        content={
+            "sucess": False,
+            "error": {"message": exc.message, "type": exc.__class__.__name__},
+        },
+    )
